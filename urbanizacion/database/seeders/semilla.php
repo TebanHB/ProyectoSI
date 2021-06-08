c8dc675cb3ffa5f166b2c0095fe480f323b98aa5
RodrigoJaldin <djaldintaboada@gmail.com>
2021-06-07 22:53:11 -0400

    hola

--- urbanizacion/database/seeders/semilla.php
+++ urbanizacion/database/seeders/semilla.php
@@ -17,23 +17,69 @@ class semilla extends Seeder
         DB::table('users')->insert([
             [
                 'name'=>'Carlos Castro Flores ',
+                'carnet'=>'4717552',
                 'email'=>'carloscortezflores03@gmail.com',
                 'password'=>'carlos1234',
-                'tipo_vendedor'=>'1',
-                'tipo_visita'=>'1',
+                'tipo_vendedor'=>'0',
+                'tipo_visita'=>'0',
                 'tipo_administrador'=>'1',
-                'tipo_cliente'=>'1'
+                'tipo_cliente'=>'0'
             ],
             [
-                
-            ],
-            [
-                
-            ]
-        ]);
-        DB::table('pago')->insert([
+                'name'=>'Karla Mamani Flores ',
+                'carnet'=>'4915532',
+                'email'=>'karlitamf5@gmail.com',
+                'password'=>'carlos1234',
 
-        ]};
+                'tipo_vendedor'=>'1',
+                'tipo_visita'=>'0',
+                'tipo_administrador'=>'0',
+                'tipo_cliente'=>'0'  
+            ],
+            [
+                'name'=>'Juan Cabrera Castro ',
+                'carnet'=>'4517562',
+                'email'=>'juancito@gmail.com',
+                'password'=>'JCC18',
+                'tipo_vendedor'=>'0',
+                'tipo_visita'=>'0',
+                'tipo_administrador'=>'0',
+                'tipo_cliente'=>'1'  
+            ],
+            [
+                'name'=>'Jose Wurner Yucra ',
+                'carnet'=>'4915532',
+                'email'=>'wurner589@gmail.com',
+                'password'=>'wurner479',
+                'tipo_vendedor'=>'0',
+                'tipo_visita'=>'0',
+                'tipo_administrador'=>'0',
+                'tipo_cliente'=>'1'  
+            ],
+            [
+                'name'=>'Camila Duran Arteaga ',
+                'carnet'=>'4915532',
+                'email'=>'camilitada@gmail.com',,
+                'password'=>'carlos1234',
+
+                'tipo_vendedor'=>'1',
+                'tipo_visita'=>'0',
+                'tipo_administrador'=>'0',
+                'tipo_cliente'=>'0'  
+            ],
+            [
+                'name'=>'Kevin Garcia Yucra ',
+                'carnet'=>'5545532',
+                'email'=>'kevingarcia45@gmail.com',
+                'password'=>'carlos1234',
+
+                'tipo_vendedor'=>'0',
+                'tipo_visita'=>'1',
+                'tipo_administrador'=>'0',
+                'tipo_cliente'=>'0'  
+            ],
+        ]);
+       
 
     }
 }
