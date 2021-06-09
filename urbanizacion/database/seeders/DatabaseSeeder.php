b6e4465bf792347bc912dab1e24c2440bdd72862
Esteban <e_s_t_e_b_a_n-hurtado@hotmail.com>
2021-06-09 10:58:37 -0400

    Seed
    # Conflicts:
    #	urbanizacion/database/seeders/semilla.php

--- urbanizacion/database/seeders/DatabaseSeeder.php
+++ urbanizacion/database/seeders/DatabaseSeeder.php
@@ -14,6 +14,21 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call(semilla::class);
+        $this->call(users::class);
+        $this->call(telefono::class);
+        $this->call(pago::class);
+        $this->call(credito::class);
+        $this->call(compromiso::class);
+        $this->call(contado::class);
+        $this->call(manzana::class);
+        $this->call(contrato::class);
+        $this->call(mora::class);
+        $this->call(cuota::class);
+        $this->call(terreno::class);
+        $this->call(ofrece::class);
+        $this->call(reserva::class);
+        $this->call(bitacora::class);
+        $this->call(notas::class);
+
     }
 }
