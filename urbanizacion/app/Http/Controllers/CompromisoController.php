5129b4c855950d2a8871ef5dae5a5f0839efeef8
Esteban <e_s_t_e_b_a_n-hurtado@hotmail.com>
2021-06-09 14:24:54 -0400

    Modelos y Controladores

---
+++ urbanizacion/app/Http/Controllers/CompromisoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Compromiso;
+use Illuminate\Http\Request;
+
+class CompromisoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Compromiso  $compromiso
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Compromiso $compromiso)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Compromiso  $compromiso
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Compromiso $compromiso)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Compromiso  $compromiso
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Compromiso $compromiso)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Compromiso  $compromiso
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Compromiso $compromiso)
+    {
+        //
+    }
+}
---
+++ urbanizacion/app/Http/Controllers/ManzanaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Manzana;
+use Illuminate\Http\Request;
+
+class ManzanaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Manzana  $manzana
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Manzana $manzana)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Manzana  $manzana
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Manzana $manzana)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Manzana  $manzana
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Manzana $manzana)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Manzana  $manzana
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Manzana $manzana)
+    {
+        //
+    }
+}
