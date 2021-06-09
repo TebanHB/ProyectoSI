f9fcd6c5d6a50a63f98c275d1acf9bee4f6c9894
camilareyes03 <camilareyes562@gmail.com>
2021-06-09 00:20:18 -0400

    yo

--- urbanizacion/database/seeders/semilla.php
+++ urbanizacion/database/seeders/semilla.php
@@ -316,7 +316,7 @@ class semilla extends Seeder
             DB::table('contado')->insert([
                 [
                     'monto'=>'42750',
-                    'descuento'=>'2250',            //5%
+                    'descuento'=>'2250',        //5%
                     'fecha_de_pago'=>'02-02-2021' 
                 ],
                 [
@@ -327,65 +327,65 @@ class semilla extends Seeder
             ]);  
             DB::table('manzana')->insert([
                 [
-                    'superficie_manzana'=>'3640'	 //10*100/terreno chico, 8*150 terreno mediano, 8*180/terreno grande
+                    'superficie_manzana'=>'3640',	 //10*100/terreno chico, 8*150 terreno mediano, 8*180/terreno grande
                     'cantidad_de_terreno'=>'26'
                 ],
                 [
-                    'superficie_manzana'=>'2900'	 //8chico - 8 mediano - 5 grande
+                    'superficie_manzana'=>'2900',	 //8chico - 8 mediano - 5 grande
                     'cantidad_de_terreno'=>'21'
                 ],
                 [
-                    'superficie_manzana'=>'3300'	//12 chico- 8 mediano - 5 grandes
+                    'superficie_manzana'=>'3300',	//12 chico- 8 mediano - 5 grandes
                     'cantidad_de_terreno'=>'25'
                 ]
             
             ]);  
             DB::table('contrato')->insert([
                 [ 
-                    'fecha_adjudicacion'=>'01-02-2018'
-                    'monto'=>'32000'
-                    'id_user'=>'2'
+                    'fecha_adjudicacion'=>'01-02-2018',
+                    'monto'=>'32000',
+                    'id_user'=>'2',
                     'codigo_pago'=>'1'
             
                 ],
                 [ 
-                    'fecha_adjudicacion'=>'01-05-2020'
-                    'monto'=>'25000'
-                    'id_user'=>'3'
+                    'fecha_adjudicacion'=>'01-05-2020',
+                    'monto'=>'25000',
+                    'id_user'=>'3',
                     'codigo_pago'=>'1'
                 ],
                 [ 
-                    'fecha_adjudicacion'=>'01-03-2019'
-                    'monto'=>'30000'
-                    'id_user'=>'4'
+                    'fecha_adjudicacion'=>'01-03-2019',
+                    'monto'=>'30000',
+                    'id_user'=>'4',
                     'codigo_pago'=>'1'
             
                 ],
                 [ 
-                    'fecha_adjudicacion'=>'01-01-2018'
-                    'monto'=>'45000'
-                    'id_user'=>'5'
+                    'fecha_adjudicacion'=>'01-01-2018',
+                    'monto'=>'45000',
+                    'id_user'=>'5',
                     'codigo_pago'=>'2'
             
                 ],
                 [ 
-                    'fecha_adjudicacion'=>'02-12-2017'
-                    'monto'=>'55000'
-                    'id_user'=>'6'
+                    'fecha_adjudicacion'=>'02-12-2017',
+                    'monto'=>'55000',
+                    'id_user'=>'6',
                     'codigo_pago'=>'2'
             
                 ],
                 [ 
-                    'fecha_adjudicacion'=>'02-02-2021'
-                    'monto'=>'42750'
-                    'id_user'=>'7'
+                    'fecha_adjudicacion'=>'02-02-2021',
+                    'monto'=>'42750',
+                    'id_user'=>'7',
                     'codigo_pago'=>'3'
             
                 ],
                 [ 
-                    'fecha_adjudicacion'=>'18-04-2021'
-                    'monto'=>'33725'
-                    'id_user'=>'8'
+                    'fecha_adjudicacion'=>'18-04-2021',
+                    'monto'=>'33725',
+                    'id_user'=>'8',
                     'codigo_pago'=>'3'
             
                 ],
