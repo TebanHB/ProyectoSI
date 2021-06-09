3f2d102ba76b903292c5c89e7111d1542b770bb9
TillWurner <85416653+TillWurner@users.noreply.github.com>
2021-06-08 23:05:49 -0400

    Cambio Jose

--- urbanizacion/database/migrations/2021_06_05_221111_create_credito_table.php
+++ urbanizacion/database/migrations/2021_06_05_221111_create_credito_table.php
@@ -19,10 +19,11 @@ class CreateCreditoTable extends Migration
             $table->integer('plazo');
             $table->integer('interes_anual');
             $table->integer('cuota_mensual');
+            $table->string('fecha_Prog');
+            $table->date('fecha_inicio');
             $table->date('fecha_ultima_cuota');
             $table->string('estado');
             $table->float('saldo_financiado');
-            $table->date('fecha_Prog');
             $table->timestamps();
         });
     }
