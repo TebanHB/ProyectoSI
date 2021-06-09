a22cca88bafe9287010e35f99d1ae76a95c210a5
RodrigoJaldin <djaldintaboada@gmail.com>
2021-06-09 01:14:49 -0400

    hoa

--- urbanizacion/database/seeders/semilla.php
+++ urbanizacion/database/seeders/semilla.php
@@ -26,13 +26,13 @@ class semilla extends Seeder
                 'tipo_administrador'=>'1',
                 'tipo_cliente'=>'0'
             ]
-        ]);
-            /*
+    
+            
             [
                 'name'=>'Carolina Duran Reyes',
                 'carnet'=>'5687890',
                 'email'=>'carolinitatubebesota12@gmail.com',
-                'password'=>'Bebota701',
+                'password'=>Hash::make('Bebota701'),
                 'tipo_vendedor'=>'0',
                 'tipo_visita'=>'0',
                 'tipo_administrador'=>'0',
@@ -42,7 +42,7 @@ class semilla extends Seeder
                 'name'=>'Juan Cabrera Castro ',
                 'carnet'=>'4517562',
                 'email'=>'juancito@gmail.com',
-                'password'=>'JCC18',
+                'password'=>Hash::make('JCC18'),
                 'tipo_vendedor'=>'0',
                 'tipo_visita'=>'0',
                 'tipo_administrador'=>'0',
@@ -52,7 +52,7 @@ class semilla extends Seeder
                 'name'=>'Jose Wurner Yucra ',
                 'carnet'=>'4915532',
                 'email'=>'wurner589@gmail.com',
-                'password'=>'wurner479',
+                'password'=>Hash::make('wurner479'),
                 'tipo_vendedor'=>'0',
                 'tipo_visita'=>'0',
                 'tipo_administrador'=>'0',
@@ -63,7 +63,7 @@ class semilla extends Seeder
                 'name'=>'Mariela Loayza Guzman',
                 'carnet'=>'4788255',
                 'email'=>'mariela123@gmail.com',
-                'password'=>'contra12',
+                'password'=>Hash::make('contra12'),
                 'tipo_vendedor'=>'0',
                 'tipo_visita'=>'0',
                 'tipo_administrador'=>'0',
@@ -74,7 +74,7 @@ class semilla extends Seeder
                 'name'=>'Fabio Fernandez',
                 'carnet'=>'4789255',
                 'email'=>'FFernandez@gmail.com',
-                'password'=>'fabiofer',
+                'password'=>Hash::make('fabiofer'),
                 'tipo_vendedor'=>'0',
                 'tipo_visita'=>'0',
                 'tipo_administrador'=>'0',
@@ -84,7 +84,7 @@ class semilla extends Seeder
                 'name'=>'Daniel Cortez Roca',
                 'carnet'=>'1894126',
                 'email'=>'danicroca12@gmail.com',
-                'password'=>'danicr12',
+                'password'=>Hash::make('danicr12'),
                 'tipo_vendedor'=>'0',
                 'tipo_visita'=>'0',
                 'tipo_administrador'=>'0',
@@ -105,6 +105,7 @@ class semilla extends Seeder
                 'name'=>'Karla Mamani Flores ',
                 'carnet'=>'4915532',
                 'email'=>'karlitamf5@gmail.com',
+                'password'=>hash::make('kar45'),
                 'tipo_vendedor'=>'1',
                 'tipo_visita'=>'0',
                 'tipo_administrador'=>'0',
@@ -114,6 +115,7 @@ class semilla extends Seeder
                 'name'=>'Camila Duran Arteaga ',
                 'carnet'=>'4915532',
                 'email'=>'camilitada@gmail.com',
+                'password'=>hash::make('camilarex1'),
                 'tipo_vendedor'=>'1',
                 'tipo_visita'=>'0',
                 'tipo_administrador'=>'0',
@@ -125,6 +127,7 @@ class semilla extends Seeder
                 'name'=>'Milagros Rodriguez Cabello',
                 'carnet'=>'5798712',
                 'email'=>'milarodriguez56@gmail.com',
+                'password'=>hash::make('mila78as'),
                 'tipo_vendedor'=>'1',
                 'tipo_visita'=>'0',
                 'tipo_administrador'=>'0',
@@ -135,7 +138,7 @@ class semilla extends Seeder
                 'name'=>'Marianela Coimbra Caballero',
                 'carnet'=>'8465684',
                 'email'=>'marianelacc@gmail.com',
-                'password'=>'mari1234',
+                'password'=>hash::make('mair592'),
                 'tipo_vendedor'=>'1',
                 'tipo_visita'=>'0',
                 'tipo_administrador'=>'0',
@@ -145,7 +148,7 @@ class semilla extends Seeder
                 'name'=>'Juan Carlos Lazo Zabala',
                 'carnet'=>'4687321',
                 'email'=>'juancarlos83@gmail.com',
-                'password'=>'juancalazo83',
+                'password'=>hash::make('juancalazo83'),
                 'tipo_vendedor'=>'0',
                 'tipo_visita'=>'1',
                 'tipo_administrador'=>'0',
@@ -156,6 +159,7 @@ class semilla extends Seeder
                 'name'=>'Cristina Duran Reyes',
                 'carnet'=>'5588890',
                 'email'=>'cristinini1003@gmail.com',
+                'password'=>hash::make('cristinini45'),
                 'tipo_vendedor'=>'0',
                 'tipo_visita'=>'1',
                 'tipo_administrador'=>'0',
@@ -423,6 +427,6 @@ class semilla extends Seeder
 
                 ]
             ]);  
-            */   
+         
     }
 }
