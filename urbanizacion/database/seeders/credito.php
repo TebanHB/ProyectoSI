9f159b1f48e61d82296ff746a48b54e10b82dd3c
camilareyes03 <camilareyes562@gmail.com>
2021-06-09 16:28:49 -0400

    .

--- urbanizacion/database/seeders/credito.php
+++ urbanizacion/database/seeders/credito.php
@@ -17,39 +17,37 @@ class credito extends Seeder
         //
         DB::table('credito')->insert([
             [
-                'cuotainicial'=>'5.760', //Total 32000 //18%
+                'cuota_inicial'=>'5760', //Total 32000 //18%
                 'plazo'=>'15',
-                'interes anual'=>'1.312', // Interes 5
-                'cuota mensual'=>'255.13', //Mensual 145,83 //Mensual con Interes 255.13
+                'interes_anual'=>'1312', // Interes 5
+                'cuota_mensual'=>'255.13', //Mensual 145,83 //Mensual con Interes 255.13
                 'fecha_Prog'=>'1 de cada mes',  //1er de cada Mes
-                'fecha inicio'=>'02-01-2018',
-                'fecha ultima cuota'=>'02-01-2032',
+                'fecha_inicio'=>'2018-02-01',
+                'fecha_ultima_cuota'=>'2032-02-01',
                 'estado'=>'Activo',
-                'saldo_financiado'=>'26.240'
+                'saldo_financiado'=>'26240'
+            ] ,
+            [
+                'cuota_inicial'=>'4500', //Total 25000 //18%
+                'plazo'=>'15',
+                'interes_anual'=>'1025', //Interes 5
+                'cuota_mensual'=>'174.31', //Mensual 88.89 //Mensual con Interes 174,3067
+                'fecha_Prog'=>'1 de cada mes',  //1er de cada Mes
+                'fecha_inicio'=>'2020-05-01',
+                'fecha_ultima_cuota'=>'2035-05-01',
+                'estado'=>'Activo',
+                'saldo_financiado'=>'16000'
             ],
             [
-                'cuotainicial'=>'4500', //Total 25000 //18%
+                'cuota_inicial'=>'5400', //Total 30000 //18%
                 'plazo'=>'15',
-                'interes anual'=>'1.025', //Interes 5
-                'cuota mensual'=>'174,31', //Mensual 88.89 //Mensual con Interes 174,3067
-                'fecha_Prog'=>'1',  //1er de cada Mes
-                'fecha inicio'=>'05-01-2020',
-                'fecha ultima cuota'=>'05-01-2035',
+                'interes_anual'=>'1230',//5
+                'cuota_mensual'=>'239.17', //Mensual 136,67 //Mensual con Interes 239,17
+                'fecha_Prog'=>'1 de cada mes',  //1er de cada Mes
+                'fecha_inicio'=>'2019-03-01',
+                'fecha_ultima_cuota'=>'2034-03-01',
                 'estado'=>'Activo',
-                'saldo_financiado'=>'16.000',
-            ],
-            [
-                'cuotainicial'=>'5.400', //Total 30000 //18%
-                'plazo'=>'15',
-                'interes anual'=>'1.230',//5
-                'cuota mensual'=>'239,17', //Mensual 136,67 //Mensual con Interes 239,17
-                'fecha_Prog'=>'1',  //1er de cada Mes
-                'fecha inicio'=>'03-01-2019',
-                'fecha ultima cuota'=>'03-01-2034',
-                'estado'=>'Activo',
-                'saldo_financiado'=>'24.600',
-
-
+                'saldo_financiado'=>'24600'
             ]
         ]);
     }
