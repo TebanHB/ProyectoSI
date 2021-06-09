4aee4dfc0e96138e07a9648b10754134c4bcb87f
TillWurner <85416653+TillWurner@users.noreply.github.com>
2021-06-09 16:06:58 -0400

    Cambios holaasa

--- urbanizacion/database/seeders/notas.php
+++ urbanizacion/database/seeders/notas.php
@@ -14,5 +14,41 @@ class notas extends Seeder
     public function run()
     {
         //
+        DB::table('notas')->insert([
+            [
+                'descripcion'=>'agrego contrato',
+                'fecha'=>'01-02-2018',
+                'id_bitacora'=>'1'
+            ], 
+            [
+                'descripcion'=>'agrego contrato',
+                'fecha'=>'01-05-2020',
+                'id_bitacora'=>'1'
+            ],
+            [
+                'descripcion'=>'agrego contrato',
+                'fecha'=>'01-03-2019',
+                'id_bitacora'=>'1'
+            ],
+            [
+                'descripcion'=>'agrego contrato',
+                'fecha'=>'01-01-2018',
+                'id_bitacora'=>'1'
+            ],
+            [
+                'descripcion'=>'agrego contrato',
+                'fecha'=>'02-12-2017',
+                'id_bitacora'=>'1'
+            ],
+            [
+                'descripcion'=>'agrego contrato',
+                'fecha'=>'02-02-2021',
+                'id_bitacora'=>'1'
+            ],
+            [
+                'descripcion'=>'agrego contrato',
+                'fecha'=>'18-04-2021',
+                'id_bitacora'=>'1'
+            ]
     }
 }
