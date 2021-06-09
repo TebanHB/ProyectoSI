5a67898f388445c2fc83e8ca38d8fdb2fe3b3256
camilareyes03 <camilareyes562@gmail.com>
2021-06-09 15:40:45 -0400

    .

--- urbanizacion/database/seeders/vende.php
+++ urbanizacion/database/seeders/vende.php
@@ -17,24 +17,39 @@ class ofrece extends Seeder
         DB::table('vende')->insert([
             [
                 'id_user'=>'9'.
-                'id_lote'=>'66',
-                'comision'=>''
+                'id_lote'=>'1',
+                'comision'=>'1600'   //credito 5% en oferta
             ],
             [
                 'id_user'=>'10'.
-                'id_lote'=>'67',
-                'comision'=>''
+                'id_lote'=>'2',
+                'comision'=>'2023.5' //contado 6%
             ],
             [
                 'id_user'=>'11'.
-                'id_lote'=>'68',
-                'comision'=>''
+                'id_lote'=>'3',
+                'comision'=>'2250'  //compromiso 5%
             ],
             [
                 'id_user'=>'12'.
-                'id_lote'=>'',
-                'comision'=>''
+                'id_lote'=>'27',
+                'comision'=>'2250'  //compromiso 5%
             ],
+            [
+                'id_user'=>'12'.
+                'id_lote'=>'40',
+                'comision'=>'1250' //credito - 5%  oferta 25000
+            ],
+            [
+                'id_user'=>'12'.
+                'id_lote'=>'48',
+                'comision'=>'1500'      //credito
+            ],
+            [
+                'id_user'=>'12'.
+                'id_lote'=>'68',
+                'comision'=>'2565'      //contado
+            ]
         ]);
     }
 }
