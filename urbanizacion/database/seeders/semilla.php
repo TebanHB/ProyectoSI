39155523450de957f84dd57b2e88e33123c6dbe3
TillWurner <85416653+TillWurner@users.noreply.github.com>
2021-06-09 00:35:00 -0400

    Cambios CAMILA4

--- urbanizacion/database/seeders/semilla.php
+++ urbanizacion/database/seeders/semilla.php
@@ -397,22 +397,22 @@ class semilla extends Seeder
                     'multa'=>'15.31',//primero
                     'retraso_dia'=>'2'
             
-                ]
+                ],
                 [ 
                     'multa'=>'26.15',//segundo 
                     'retraso_dia'=>'5'
 
-                ]
+                ],
                 [ 
                     'multa'=>'30.62',// primero
                     'retraso_dia'=>'4'
 
-                ]
+                ],
                 [ 
                     'multa'=>'43.05',//tercero
                     'retraso_dia'=>'6'
 
-                ]
+                ],
                 [ 
                     'multa'=>'21.53',//tercero
                     'retraso_dia'=>'3'
