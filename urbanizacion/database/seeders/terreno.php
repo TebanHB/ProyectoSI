aa81f1ee1f4a67469947c06e8b0563c43dee46d9
RodrigoJaldin <djaldintaboada@gmail.com>
2021-06-09 12:23:29 -0400

    hoa

--- urbanizacion/database/seeders/terreno.php
+++ urbanizacion/database/seeders/terreno.php
@@ -155,7 +155,13 @@ class terreno extends Seeder
                 'superficie_terreno'=>'180',
                 'precio'=>'40000',
                 'estado_terreno'=>'libre'
-            ]
+            ],
+            [
+                'id_manzana'=>'1',		
+                'superficie_terreno'=>'180',
+                'precio'=>'40000',
+                'estado_terreno'=>'libre'
+            ],
             // Manzana 2
            //grandes 4
            [
@@ -442,62 +448,63 @@ class terreno extends Seeder
             'estado_terreno'=>'libre',
         
         ],
+        
         [
-            'id_contrato'=>'1'
-            'id_manzana'=>'1'
-            'id_users'=>'2'
-            'superficie_terreno'=>'150'
-            'precio'=>'32000'
-            'estado_terreno'=>'ocuapdo'
+            'id_contrato'=>'1',
+            'id_manzana'=>'1',
+            'id_users'=>'2',
+            'superficie_terreno'=>'150',
+            'precio'=>'32000',
+            'estado_terreno'=>'ocupado',
            
         ],
         [
-            'id_contrato'=>'2'
-            'id_manzana'=>'2'
-            'id_users'=>'3'
-            'superficie_terreno'=>'100'
-            'precio'=>'25000'         //precio super oferta
-            'estado_terreno'=>'ocuapdo'
+            'id_contrato'=>'2',
+            'id_manzana'=>'2',
+            'id_users'=>'3',
+            'superficie_terreno'=>'100',
+            'precio'=>'25000'    ,     //precio super oferta
+            'estado_terreno'=>'ocupado'
         ],
         [
-            'id_contrato'=>'3'
-            'id_manzana'=>'3'
-            'id_users'=>'4'
-            'superficie_terreno'=>'100'
-            'precio'=>'30000'
-            'estado_terreno'=>'ocuapdo'
+            'id_contrato'=>'3',
+            'id_manzana'=>'3',
+            'id_users'=>'4',
+            'superficie_terreno'=>'100',
+            'precio'=>'30000',
+            'estado_terreno'=>'ocupado'
         ],
         [
-            'id_contrato'=>'4'
-            'id_manzana'=>'1'
-            'id_users'=>'5'
-            'superficie_terreno'=>'180'
-            'precio'=>'45000'
-            'estado_terreno'=>'ocuapdo'
+            'id_contrato'=>'4',
+            'id_manzana'=>'1',
+            'id_users'=>'5',
+            'superficie_terreno'=>'180',
+            'precio'=>'45000',
+            'estado_terreno'=>'ocupado',
         ],
         [
-            'id_contrato'=>'5'
-            'id_manzana'=>'2'
-            'id_users'=>'6'
-            'superficie_terreno'=>'180'
-            'precio'=>'45000'
-            'estado_terreno'=>'ocuapdo'
+            'id_contrato'=>'5',
+            'id_manzana'=>'2',
+            'id_users'=>'6',
+            'superficie_terreno'=>'180',
+            'precio'=>'45000',
+            'estado_terreno'=>'ocupado',
         ],
         [
-            'id_contrato'=>'6'
-            'id_manzana'=>'3'
-            'id_users'=>'7'
-            'superficie_terreno'=>'180'
-            'precio'=>'42750'
-            'estado_terreno'=>'ocuapdo'
+            'id_contrato'=>'6',
+            'id_manzana'=>'3',
+            'id_users'=>'7',
+            'superficie_terreno'=>'180',
+            'precio'=>'42750',
+            'estado_terreno'=>'ocupado',
         ],
         [
-            'id_contrato'=>'7'
-            'id_manzana'=>'1'
-            'id_users'=>'8'
-            'superficie_terreno'=>'150'
-            'precio'=>'33725'
-            'estado_terreno'=>'ocuapdo'
+            'id_contrato'=>'7',
+            'id_manzana'=>'1',
+            'id_users'=>'8',
+            'superficie_terreno'=>'150',
+            'precio'=>'33725',
+            'estado_terreno'=>'ocupado',
         ],
 
 
