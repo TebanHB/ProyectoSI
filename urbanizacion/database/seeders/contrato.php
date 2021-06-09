a675e48e66d10482dcb0cee5f3439923843e85e6
angelica2309 <78491584+angelica2309@users.noreply.github.com>
2021-06-09 16:33:49 -0400

    fechaaaaa

--- urbanizacion/database/seeders/contrato.php
+++ urbanizacion/database/seeders/contrato.php
@@ -17,48 +17,48 @@ class contrato extends Seeder
         //
         DB::table('contrato')->insert([
             [
-                'fecha_adjudicacion'=>'01-02-2018',
+                'fecha_adjudicacion'=>'2018-02-01',//'01-02-2018',
                 'monto'=>'32000',
                 'id_user'=>'2',
                 'codigo_pago'=>'1'
 
             ],
             [
-                'fecha_adjudicacion'=>'01-05-2020',
+                'fecha_adjudicacion'=>'2020-05-01',//'01-05-2020',
                 'monto'=>'25000',
                 'id_user'=>'3',
                 'codigo_pago'=>'1'
             ],
             [
-                'fecha_adjudicacion'=>'01-03-2019',
+                'fecha_adjudicacion'=>'2019-03-01',//'01-03-2019',
                 'monto'=>'30000',
                 'id_user'=>'4',
                 'codigo_pago'=>'1'
 
             ],
             [
-                'fecha_adjudicacion'=>'01-01-2018',
+                'fecha_adjudicacion'=>'2018-01-01',//'01-01-2018',
                 'monto'=>'45000',
                 'id_user'=>'5',
                 'codigo_pago'=>'2'
 
             ],
             [
-                'fecha_adjudicacion'=>'02-12-2017',
+                'fecha_adjudicacion'=>'2017-12-02',//'02-12-2017',
                 'monto'=>'45000',
                 'id_user'=>'6',
                 'codigo_pago'=>'2'
 
             ],
             [
-                'fecha_adjudicacion'=>'02-02-2021',
+                'fecha_adjudicacion'=>'2021-02-02',//'02-02-2021',
                 'monto'=>'42750',
                 'id_user'=>'7',
                 'codigo_pago'=>'3'
 
             ],
             [
-                'fecha_adjudicacion'=>'18-04-2021',
+                'fecha_adjudicacion'=>'2021-04-18',//'18-04-2021',
                 'monto'=>'33725',
                 'id_user'=>'8',
                 'codigo_pago'=>'3'
