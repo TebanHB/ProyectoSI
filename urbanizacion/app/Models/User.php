bd89c9293148768017a1c480b83e334ff8c74af3
Esteban <e_s_t_e_b_a_n-hurtado@hotmail.com>
2021-06-10 00:28:37 -0400

    Modelos y Controladores

--- urbanizacion/app/Models/User.php
+++ urbanizacion/app/Models/User.php
@@ -20,6 +20,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'tipo_vendedor',
+        'tipo_visita',
+        'tipo_cliente',
+        'tipo_administrador',
     ];
 
     /**
