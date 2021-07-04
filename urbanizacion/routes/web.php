63f707c7481e5e3e53b4d35ffe4a66c3b6ffbeb1
Esteban <e_s_t_e_b_a_n-hurtado@hotmail.com>
2021-07-04 18:03:28 -0400

    Auth

--- urbanizacion/routes/web.php
+++ urbanizacion/routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/loginprueba',function (){
+    return view('newlogin/login.blade.php');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
