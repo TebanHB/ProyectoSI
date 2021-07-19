496ba09d9ca050958610dad56d6c4a868c4180fb
camilareyes03 <camilareyes562@gmail.com>
2021-07-19 00:28:15 -0400

    cam

--- urbanizacion/resources/views/contrato/index.blade.php
+++ urbanizacion/resources/views/contrato/index.blade.php
@@ -22,19 +22,21 @@
     </ul>
     <table class="table table-striped">
         <thead>
-            <th>fecha_adjudicacion</th>
-            <th>monto</th>
             <th>Nombre</th>
             <th>Codigo de Pago</th>
+            <th>fecha_adjudicacion</th>
+            <th>monto</th>
+            
             {{-- <th>user</th> --}}
       </thead>
       <tbody>
           @foreach ($contratos as $contrato)
              <tr>
+                 <td>{{($contrato->id_user == null)? "--": $contrato->id_user}}</td>
+                  <td>{{($contrato->codigo_pago == null)? "--": $contrato->codigo_pago}}</td>
                   <td>{{($contrato->fecha_adjudicacion == null)? "--": $contrato->fecha_adjudicacion}}</td>
                   <td>{{($contrato->monto == null)? "--": $contrato->monto}}</td>
-                  <td>{{($contrato->id_user == null)? "--": $contrato->id_user}}</td>
-                  <td>{{($contrato->codigo_pago == null)? "--": $contrato->codigo_pago}}</td>
+                  
                   {{-- <td>{{$contrato->users->name}}</td>                 --}}
                   <td>        
                </tr> 
