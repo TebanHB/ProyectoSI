e9b1e3f3598b2eff829001ce8c85b483e0be7d78
camilareyes03 <camilareyes562@gmail.com>
2021-07-18 21:50:22 -0400

    cam

---
+++ urbanizacion/resources/views/terreno/index.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('content')
+{{-- esto es pa mostrar errores --}}
+@if ($errors->count() > 0)
+<div class="alert alert-danger">
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{$error}}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
+
+<br>
+
+<div class="container">
+    <br>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item"><h2>Terrenos</h2></li>
+        {{-- <li class="list-group-item"><a href= {{route('user.register') }} ><button type="button" class="btn btn-success btn-lg btn-block">Nuevo usuario</button></a></li> --}}
+    </ul>
+    <table class="table table-striped">
+        <thead>
+              <th>id_contrato</th>
+              <th>id_manzana</th>
+              <th>id_users</th>
+              <th>superficie_terreno</th>
+              <th>precio</th>
+              <th>estado_terreno</th>
+            
+        </thead>
+        <tbody>
+            @foreach ($terrenos as $terreno)
+               <tr>
+                    <td>{{($terreno->id_contrato == null)? "--": $terreno->id_contrato }}</td>
+                    <td>{{($terreno->id_manzana == null)? "--": $terreno->id_manzana}}</td>
+                    <td>{{($terreno->id_users == null)? "--": $terreno->id_users }}</td>
+                    <td>{{($terreno->superficie_terreno == null)? "--": $terreno->superficie_terreno}}</td>
+                    <td>{{($terreno->precio == null)? "--": $terreno->precio }}</td>
+                    <td>{{($terreno->estado_terreno == null)? "--": $terreno->estado_terreno}}</td>
+    
+                    {{-- <td>
+                        <a href="{{route('user.permissions',$user->id)}}"><button type="button" class="btn btn-warning">Roles</button></a>
+                    </td> --}}
+               </tr> 
+            @endforeach
+        </tbody>
+    </table>
+    {{-- <div class="table table-striped">{{$pagos->links()}}</div> --}}
+</div>
+@endsection
---
+++ urbanizacion/resources/views/vende/index.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+{{-- esto es pa mostrar errores --}}
+@if ($errors->count() > 0)
+<div class="alert alert-danger">
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{$error}}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
+
+<br>
+
+<div class="container">
+    <br>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item"><h2>Terrenos vendidos</h2></li>
+        {{-- <li class="list-group-item"><a href= {{route('user.register') }} ><button type="button" class="btn btn-success btn-lg btn-block">Nuevo usuario</button></a></li> --}}
+    </ul>
+    <table class="table table-striped">
+        <thead>
+              <th>id_user</th>
+              <th>id_lote</th>
+              <th>comision</th>
+             
+            
+        </thead>
+        <tbody>
+            @foreach ($ventas as $vende)
+               <tr>
+                    <td>{{($vende->id_user == null)? "--": $vende->id_user }}</td>
+                    <td>{{($vende->id_lote == null)? "--": $vende->id_lote}}</td>
+                    <td>{{($vende->comision == null)? "--": $vende->comision }}</td>
+            
+                    {{-- <td>
+                        <a href="{{route('user.permissions',$user->id)}}"><button type="button" class="btn btn-warning">Roles</button></a>
+                    </td> --}}
+               </tr> 
+            @endforeach
+        </tbody>
+    </table>
+    {{-- <div class="table table-striped">{{$pagos->links()}}</div> --}}
+</div>
+@endsection
