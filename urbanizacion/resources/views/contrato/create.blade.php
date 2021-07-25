66676aadbe4aa6c45147847406763bbb7ae15cec
RodrigoJaldin <djaldintaboada@gmail.com>
2021-07-25 18:56:29 -0400

    ¨faf¨

--- urbanizacion/resources/views/contrato/create.blade.php
+++ urbanizacion/resources/views/contrato/create.blade.php
@@ -16,108 +16,137 @@
         </ul>
     </div>
     @endif
-    <div class="container"> 
-        <h1>Crear contrato</h1> 
+    <h1 class="Titulo_Contrato">Crear contrato</h1> 
+    <div class="containercontrato"> 
+       
         <div class="card-body">
             <form method="POST" action="{{ route('contrato.store') }}" enctype="multipart/form-data">
                 @csrf
-                <div class="contrato">
+                <div id="contratoform">
                     <label for=nro_abj>Nro de Adjudicacion: </label>
                     <input type="text" id="nro_abj"> 
                     <label for="fecha">Fecha: </label>
                     <input type="date"id="fecha">
+                    <br>
                     <label for="cot">Cotizacion: </label>
                     <input type="text"id="cot">
-                  </div>
-                  <br> <br>
-
-                <div class="Datos_cliente">
+             
+                </div>
+                  <div id="Metodo_de_pago">
+                    <h5>Forma de pago: </h5> 
+                    <input type="radio" name="pago" id="compromiso1">
+                    <label for = "compromiso1">compromiso</label>
+                    <input type="radio" name="pago" id="contado1">
+                    <label for = "contado1">contado</label>
+                     <input type="radio" name="pago" id="credito1">
+                     <label for = "credito1">credito</label>
+                </div>
+                <div id="Datos_cliente">
                     <label for=nomb>Nombre y Apellido: </label>
                     <input type="text" id="nomb"> 
+                    <br>
                     <label for="carnet">Carnet: </label> 
                     <input type="text" id="carnet"> 
                     <label for="tel">Telefono: </label> 
                     <input type="text" id="tel"> 
                 </div>
-            <br> <br>
 
-                <div class="Datos_terreno">
+                <div id="Datos_terreno">
                     <label for=nro_terreno>Nro del Terreno : </label>
-                    <input type="text" id="nro_terreno"> 
+                    <input type="text" id="nro_terreno">
+                    <br>
                     <label for="manzana">Manzana: </label> 
-                    <input type="text" id="manzana">  </br>
+                    <input type="text" id="manzana">  
                     <label for="precio">Precio: </label>
                     <input type="text" id="precio"> 
                     <label for=Superficie>Superficie : </label>
                     <input type="text" id="Superficie"> 
 
                 </div>
-                <br> <br>
 
-                <div class="Metodo de pago">
-                    
-                    <br>Forma de pago: </br>
-                    <input type="radio" name="" id="">compromiso
-                    <input type="radio" name="" id="">contado 
-                     <input type="radio" name="" id="">credito
-                </div>
-                <br> <br>
+                
 
-                <div class="Datos_pago">
-                    <div class="pago_credito">
+                <div id="Datos_pago">
+                    <div id="pago_credito">
                         <h5>Credito</h5>
                         <label for=cuota_inicial>Cuota Inicial: </label>
                         <input type="text" id="cuota_inicial"> 
+                        <br>                        <br>
+
+
                         <label for=Plazo>Plazo: </label>
                         <input type="text" id="Plazo"> 
+                        <br>                        <br>
+
+
                         <label for=Interes_anual>Interes Anual : </label>
                         <input type="text" id="Interes_anual"> 
-                        <br>
+                        <br>                        <br>
                         <label for=cuota_mensual>Cuota Mensual: </label>
                         <input type="text" id="cuota_mensual"> 
+                        <br>                        <br>
+
+
                         <label for=Fecha_programada>Fecha Programada: </label>
                         <input type="text" id="Fecha_programada"> 
+                        <br>        <br>
+
+
                         <label for=Fecha_inicio>Fecha Inicio: </label>
                         <input type="date" id="cuota_inicial"> 
-                        <br>
+                        <br>                        <br>
+                    
 
                         <label for=Fecha_ult>Fecha Ultima Cuota: </label>
                         <input type="date" id="Fecha_ult">
+                        <br>                        <br>
+
+
                         <label for=saldo_fin>Saldo Financiado: </label>
                         <input type="date" id="saldo_fin">
                     </div>   
-                    <br> <br>
-                    <div class="compromiso">
+                    <div id="compromiso">
                         <h5>Compromiso</h5>
 
                         <label for=monto_pag>Monto Pagado: </label>
                         <input type="text" id="monto_pag"> 
+                        <br>                        <br>
+
                         <label for=monto_pagar>Monto a Pagar: </label>
                         <input type="text" id="monto_pagar">
-                        <br>
+                        <br>                        <br>
+
                         <label for=Fecha_gar>Fecha Pago Garantia: </label>
                         <input type="date" id="Fecha_gar">
+                        <br>                        <br>
+
                         <label for=Fecha_apag>Fecha a Pagar: </label>
                         <input type="date" id="Fecha_apag">
                     </div>
-                    <br> <br>
 
-                    <div clas="contado">
+                    <div id="contadocontrato">
                         <h5>Contado</h5>
                         <label for=monto>Monto: </label>
                         <input type="text" id="monto"> 
                         <label for=descuento>Descuento: </label>
                         <input type="text" id="descuento"> 
+                        <br>                        <br>
+
                         <label for=fecha>Fecha de Pago: </label>
                         <input type="date" id="fecha"> 
 
                     </div>
                 </div>  
             </form>
+            
         </div>
+        
+    
     </div>  
-   
-    <button class="botonGuardar">Guardar Contrato</button>
-    <button class="botoncancelar">Cancelar</button>
+    <div class="botonescontrato">
+        <button id="botonGuardar">Guardar Contrato</button>
+        <button id="botoncancelar">Cancelar</button>
+    </div>
+    
 
 @endsection
