47cd8e228164eb6b25462dadd62c6ca2d71254c4
Esteban <e_s_t_e_b_a_n-hurtado@hotmail.com>
2021-07-25 21:07:27 -0400

    Correcion

--- urbanizacion/app/Models/User.php
+++ urbanizacion/app/Models/User.php
@@ -53,15 +53,26 @@ class User extends Authenticatable
     public function terreno(){
         return $this->hasMany(Terreno::class,'id_users');
     }
-   
+
     public function vende(){
         return $this->hasMany(Vende::class,'id_user');
     }
     public function bitacora(){
         return $this->hasOne(Bitacora::class,'id_user');
     }
-     
+
     public function reserva(){
         return $this->hasMany(Reserva::class,'id_user');
+    }
+    public function rango(){
+        if($this->tipo_administrador==1)
+            return "Admin";
+        elseif($this->tipo_vendedor==1)
+            return "Vendedor";
+        elseif($this->tipo_cliente==1)
+            return "Cliente";
+        else
+            return "Visita";
+
     }
 }
--- urbanizacion/resources/views/layouts/app.blade.php
+++ urbanizacion/resources/views/layouts/app.blade.php
@@ -16,65 +16,128 @@
 			<label for="checkbox">
 				<i id="navbtn" class="fa fa-bars" aria-hidden="true"></i>
 			</label>
-		</h2>	
+		</h2>
 		<i class="fa fa-user" aria-hidden="true"></i>
 	</header>
-		
 
-	
-	
+
+
+
 	<div class="body">
 		<nav class="side-bar">
 			<div class="user-p">
+                <h4>{{Auth::user()->rango()}}</h4>
 				@can('admin')
-                <h4>ADMIN</h4>
+            <!--    <h4>ADMIN</h4> -->
                 @endcan
 				<h4 >{{ Auth::user()->name }}</h4>
+
+
 			</div>
 			<ul>
+                @if(Auth::user()->tipo_administrador==1)
 				<li>
 					<a href="{{ route('user.index') }}">
 						<i class="fa fa-eye" aria-hidden="true"></i>
-						<span>Ver Usuarios</span>
+						<span>Gestionar usuarios</span>
 					</a>
 				</li>
+                @endif
+                    @if(Auth::user()->tipo_vendedor==1)
+                        <li>
+                            <a href="#">
+                                <i class="fa fa-eye" aria-hidden="true"></i>
+                                <span>Ver mis ventas</span>
+                            </a>
+                        </li>
+                    @endif
+                @if(Auth::user()->tipo_cliente==1 or Auth::user()->tipo_visita==1)
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-eye" aria-hidden="true"></i>
+                            <span>Ver mis visitas programadas</span>
+                        </a>
+                    </li>
+                @endif
+                @if(Auth::user()->tipo_cliente==1)
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-eye" aria-hidden="true"></i>
+                            <span>Ver todos mis contratos</span>
+                        </a>
+                    </li>
+                @endif
+                @if(Auth::user()->tipo_cliente==1)
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-eye" aria-hidden="true"></i>
+                            <span>Ver kardex</span>
+                        </a>
+                    </li>
+                @endif
+                @if(Auth::user()->tipo_administrador==1)
 				<li>
 					<a href="{{route('contrato.index')}}">
 						<i class="fa fa-eye" aria-hidden="true"></i>
-						<span>Ver todos los Contratos</span>
+						<span>Ver todos los contratos</span>
 					</a>
 				</li>
+                    @endif
+                    @if(Auth::user()->tipo_administrador==1)
                 <li>
                     <a href="{{route('contrato.pendiente')}}">
                         <i class="fa fa-eye" aria-hidden="true"></i>
                         <span>Ver Contratos pendientes</span>
                     </a>
                 </li>
+                    @endif
                 <hr class="sidebar-divider">
+                    @if(Auth::user()->tipo_administrador==1 or Auth::user()->tipo_vendedor==1)
 				<li>
 					<a href="{{route('contrato.create')}}">
 						<i class="fa fa-cog" aria-hidden="true"></i>
 						<span>Agregar Nuevo Contrato</span>
 					</a>
 				</li>
+                    @endif
+                    @if(Auth::user()->tipo_administrador==1 or Auth::user()->tipo_vendedor==1)
 				<li>
 					<a href="{{route('user.create')}}">
 						<i class="fa fa-cog" aria-hidden="true"></i>
 						<span>Agregar Nuevo Usuario</span>
 					</a>
 				</li>
+                    @endif
+                    @if(Auth::user()->tipo_administrador==1 or Auth::user()->tipo_vendedor==1)
+                        <li>
+                            <a href="#">
+                                <i class="fa fa-cog" aria-hidden="true"></i>
+                                <span>Agregar nueva visita</span>
+                            </a>
+                        </li>
+                    @endif
+                    @if(Auth::user()->tipo_cliente==1 or Auth::user()->tipo_visita==1)
+                        <li>
+                            <a href="#">
+                                <i class="fa fa-cog" aria-hidden="true"></i>
+                                <span>Agendar nueva visita</span>
+                            </a>
+                        </li>
+                    @endif
 				<li>
 					<a href="{{route('manzana.index')}}">
 						<i class="fa fa-cog" aria-hidden="true"></i>
 						<span>Ver Manzanas</span>
 					</a>
 				</li>
+                    @if(Auth::user()->tipo_administrador==1)
 				<li>
 					<a href="{{route('vende.index')}}">
 						<i class="fa fa-cog" aria-hidden="true"></i>
 						<span>Ver Ventas</span>
 					</a>
 				</li>
+                    @endif
 				<li>
 					<a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
 						<i class="fa fa-power-off" aria-hidden="true"></i>
