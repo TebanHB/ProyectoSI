d352433ac7b018904b0ee304e36c1ccbf9ea2d6b
Esteban <e_s_t_e_b_a_n-hurtado@hotmail.com>
2021-07-26 00:35:03 -0400

    Correcion

--- urbanizacion/resources/views/user/create.blade.php
+++ urbanizacion/resources/views/user/create.blade.php
@@ -22,36 +22,7 @@
         <div class="card-body">
             <form method="POST" action="{{ route('user.store') }}" enctype="multipart/form-data">
                 @csrf
-<<<<<<< HEAD
-                <div class="usuarioformulariocreate">
-                        <label for=name>Nombre Completo: </label>
-                        <input type="text" id="name">
-                        <br> <br>
-                        <label for="carnet">Carnet: </label>
-                        <input type="text"id="carnet">
-                        <br> <br>
-                        <label for="email">Correo Electronico: </label>
-                        <input type="email"id="email">
-                        <br> <br>
-                        <label for="password">Contraseña: </label>
-                        <input type="password"id="password">
-                        <br> <br>
-                        <label for="verif">Verificar Contraseña: </label>
-                        <input type="password"id="verif">
-    
-                        <br> <br>
-                        <input type="checkbox" name="" id="Administrador1">
-                        <label for="Administrador1">Administrador</label>
-                        <input type="checkbox" name="" id="Vendedor1">
-                        <label for="Vendedor1">Vendedor</label>
-                        <input type="checkbox" name="" id="Cliente1">
-                        <label for="Cliente1">Cliente</label>
-                   
-                    </div>
-                    <button type="submit" class="btn btn-info ">
-                        {{ __('Registrar') }}
-                    </button>
-=======
+
                 <div class="usuario">
                     <label for=name>Nombre Completo: </label>
                     <input type="text" name="name" id="name">
@@ -70,9 +41,9 @@
 
                     <br> <br>
                 </div>
-                        
+
                         <div id="tipousuario">
-                            <h5>Tipo de usuario: </h5> 
+                            <h5>Tipo de usuario: </h5>
                             <input type="radio" name="tipo_cliente" id="cliente">
                             <label for = "tipo_cliente">Cliente</label>
                             <input type="radio" name="tipo_vendedor" id="vendedor">
@@ -85,7 +56,6 @@
                             <i class="fas fa-heartbeat"></i>
                         </button>
 
->>>>>>> 0a62a1da43d1204bdc58023cd3a6b2dc49af5616
             <!--    <a href="{{}}" class="btn btn-success">guardar</a>  Este es boton para actualizar -->
             </form>
         </div>
