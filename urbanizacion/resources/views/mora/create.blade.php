42363fcfcff2341b1e5474eac11ec79bb63e8453
angelica2309 <78491584+angelica2309@users.noreply.github.com>
2021-07-27 21:17:00 -0400

    loquesea

--- urbanizacion/resources/views/mora/create.blade.php
+++ urbanizacion/resources/views/mora/create.blade.php
@@ -10,14 +10,21 @@
 @endif
 @section('content')
 <div class="container">
+    <div class="card-body">
+        <h1 class="Titulo_Contrato">Crear Mora</h1>
 <form class="container" method="POST" action="{{ route('mora.store') }}" enctype="multipart/form-data">
     @csrf
 
     <label for="multa">Multa: </label>
     <input type="number" id="multa">
+    <br><br>
     <label for="retraso_dia">Retraso de días: </label>
     <input type="number" id="retraso_dia">
-
+    <br><br> <br>
+    <button type="submit" class="botonesmora ">
+        {{ __('Guardar') }}
+   </button>
 </form>
+
 </div>
 @endsection
