c7ecea4f35177e912aa8bb5b5d7f90b923a0cedc
Esteban <e_s_t_e_b_a_n-hurtado@hotmail.com>
2021-08-02 20:16:04 -0400

    Correcion
    # Conflicts:
    #	urbanizacion/resources/views/layouts/app.blade.php

--- urbanizacion/resources/views/layouts/app.blade.php
+++ urbanizacion/resources/views/layouts/app.blade.php
@@ -26,6 +26,7 @@
 
 	<div class="body">
 		<nav class="side-bar">
+            @if(Auth::user()!=null)
 			<div class="user-p">
                 <h4>{{Auth::user()->rango()}}</h4>
 				    @can('admin')
@@ -33,8 +34,9 @@
                 @endcan
 				<h4 >{{ Auth::user()->name }}</h4>
 			</div>
+
 			<ul>
-                
+
                 @if(Auth::user()->tipo_administrador==1)
 
                     <li>
@@ -105,7 +107,7 @@
                     </li>
 
                     <hr class="sidebar-divider">
-            
+
                     <li>
 
                         <a href="{{route('manzana.index')}}">
@@ -135,7 +137,7 @@
                             <span>ver reservas</span>
                         </a>
                     </li>
-                    
+
 
                     <li>
                         <a href="#">
@@ -233,7 +235,7 @@
 
                 <!--    <h4>VISITA</h4> -->
 
-                    
+
                 @if(Auth::user()->tipo_visita==1)
                         <li>
                             <a href="#">
@@ -241,6 +243,7 @@
                                 <span>Agendar nueva visita</span>
                             </a>
                         </li>
+
                         <hr class="sidebar-divider">
                         <li>
                             <a href="#">
@@ -250,7 +253,26 @@
                         </li>
                         <hr class="sidebar-divider">
                 @endif
-               
+
+
+                    @endif
+				<li>
+
+					<a href="{{route('manzana.index')}}">
+						<i class="fa fa-cog" aria-hidden="true"></i>
+						<span>Ver Manzanas</span>
+					</a>
+				</li>
+                    @if(Auth::user()->tipo_administrador==1 or Auth::user()->tipo_vendedor==1)
+				<li>
+					<a href="{{route('vende.index')}}">
+						<i class="fa fa-cog" aria-hidden="true"></i>
+						<span>Ver Ventas</span>
+					</a>
+				</li>
+                    @endif
+                    @endif
+
 				<li>
 					<a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
 						<i class="fa fa-power-off" aria-hidden="true"></i>
