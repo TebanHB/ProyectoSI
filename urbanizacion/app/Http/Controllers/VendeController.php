b98b97ec6c3e8f09cc933f244870f052269a7122
camilareyes03 <camilareyes562@gmail.com>
2021-08-11 03:49:06 -0400

    cam

--- urbanizacion/app/Http/Controllers/VendeController.php
+++ urbanizacion/app/Http/Controllers/VendeController.php
@@ -27,7 +27,6 @@ class VendeController extends Controller
         $ventas->load('user');
         return view('vende.index',compact('ventas'));
     }
-    
     /**
      * Show the form for creating a new resource.
      *
